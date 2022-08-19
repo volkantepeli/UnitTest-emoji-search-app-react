6b84a7be51e6ddb23ca814af67b84217821b3e4d
volkantepeli <volkan.tepelii@gmail.com>
2022-08-19 16:34:50 +0300

    tests were done

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
-Emoji Search
----
+# UnitTest
 
-Created with *create-react-app*. See the [full create-react-app guide](https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md).
+## Project used for testing
+```
+https://github.com/ahfarmer/emoji-search
+```
 
+## Tests done
+```
+- The test that will check if the header is rendered successfully
+- Test that will check if the emoji list is rendered successfully when the app is first opened
+- Test that will check that when a filter is done, the emoji list is re-rendered according to that filter
+- When clicking any emoji on the list, the test that will check that the relevant emoji is copied
+```
 
-
-Install
----
-
-`npm install`
-
-
-
-Usage
----
-
-`npm start`
+## Used technologies
+<img align="left" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/react/react.png" width="25" height="25" />
+<img align="left" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png" width="25" height="25" />
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import { screen, render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
+import Header from "./Header.js";
+import App from "./App.js";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+describe("Test for patika.dev", () => {
+  test(" Tittle render ", () => {
+    render(<Header />);
+    const head = screen.getByText("Emoji Search");
+    expect(head).toBeInTheDocument();
+  });
+  test("Emoji list render ", () => {
+    render(<App />);
+    const items = screen.getAllByText(/Click to copy emoji/i);
+    const item = screen.getByText("Relaxed");
+    expect(item).toBeInTheDocument();
+    expect(items.length).toEqual(20);
+  });
+  test("Filter render ", () => {
+    render(<App />);
+    let input = screen.getByPlaceholderText("Search for emoji");
+    userEvent.type(input, "yu");
+    let item = screen.getByText("Yum");
+    expect(item).toBeInTheDocument();
+  });
+  test("Emoji Copy ", () => {
+    render(<App />);
+    const clicks = screen.getAllByTestId("row");
+    expect(clicks[0]).toHaveAttribute("data-clipboard-text");
+  });
 });
